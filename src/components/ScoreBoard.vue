ef3a06a47addc3027f544d107b0292833d1b1f62
rbwolf <rdbaerwolf@gmail.com>
2021-12-23 20:51:39 -0800

    Add countup

--- src/components/ScoreBoard.vue
+++ src/components/ScoreBoard.vue
@@ -4,16 +4,22 @@
       v-for="(team, i) in teams"
       :key="team.id"
       class="col text-center">
-      <h1>{{ scores[i] }}</h1>
+      <h1>
+        <ICountUp :endVal="scores[i]"/>
+      </h1>
       <h3>{{ team.name }}</h3>
-      <h6>{{ bags[i] }} bag{{ bags[i] !== 1 ? 's' : '' }}</h6>
+      <h6>
+        <ICountUp :endVal="bags[i]"/> bag{{ bags[i] !== 1 ? 's' : '' }}</h6>
     </div>
   </div>
 </template>
 
 <script>
+  import ICountUp from 'vue-countup-v2';
+
   export default {
     name: 'ScoreBoard',
+    components: { ICountUp },
     props: {
       teams: {
         type: Array,
