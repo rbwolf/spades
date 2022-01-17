ca7f481099682a0c675f1a1f415bdad920136933
rbwolf <rdbaerwolf@gmail.com>
2022-01-16 17:12:09 -0800

    Add google analytics

--- src/main.js
+++ src/main.js
@@ -5,10 +5,15 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import './registerServiceWorker'
+import VueGtag from "vue-gtag";
 
 Vue.config.productionTip = false
 Vue.use(Vuex)
 Vue.use(VueRouter)
+
+Vue.use(VueGtag, {
+  config: { id: 'UA-210378975-2' }
+});
 
 const appStore = new Vuex.Store(store)
 
