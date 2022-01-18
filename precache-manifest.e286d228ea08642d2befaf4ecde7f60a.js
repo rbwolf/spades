434647fc072c19a64e44c979b54c7c572c7a970e
rbwolf <rdbaerwolf@gmail.com>
2022-01-17 22:00:26 -0800

    deploy

---
+++ precache-manifest.e286d228ea08642d2befaf4ecde7f60a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4440a48c3e57a5208e8b",
+    "url": "/spades/css/app.023d1045.css"
+  },
+  {
+    "revision": "91f468d2966f09221214eea33904815a",
+    "url": "/spades/index.html"
+  },
+  {
+    "revision": "4440a48c3e57a5208e8b",
+    "url": "/spades/js/app.f5b3e143.js"
+  },
+  {
+    "revision": "27191e7248e0050bd758",
+    "url": "/spades/js/chunk-vendors.66f7d507.js"
+  },
+  {
+    "revision": "16513002ea5a13ad4e1a2280708925f3",
+    "url": "/spades/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/spades/robots.txt"
+  }
+]);
